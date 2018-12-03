f2e30783e165825eaf6bf141c4f954b4eca3cf64
mitchya1 <mitchell.anderson@colorado.edu>
2018-12-03 16:06:43 -0700

    broken. But re-written to be less shitty. Still, broken.

--- logging-server.go
+++ logging-server.go
@@ -3,9 +3,10 @@ package main
 import (
 	"encoding/json"
 	"net/http"
-	"fmt"
+	//"fmt"
 	"os"
 	"github.com/nlopes/slack"
+	"bufio"
 )
 
 func error_func(e error) {
@@ -38,77 +39,106 @@ func checkAuth(response_ http.ResponseWriter, request *http.Request) {
 	}
 	
 	// Create map of allowed auth tokens
-	allowed_tokens := map[string] bool {
+	/* allowed_tokens := map[string] bool {
 		"ABCDEF" : true,
 		"HIJKLM": true,
+	}*/
+
+	// Create variable for incoming auth token
+	auth_token := json_data.Auth
+
+	// Create slice for allowed tokens
+	var allowed_tokens []string
+
+	// Open allowed tokens file
+	file, err := os.Open("/path/to/allowed_tokens.txt")
+
+	// If opening file fails, fail
+	if err != nil {
+		error_func(err)
 	}
 
+	// Close file
+	defer file.Close()
+
+	// Initialize file scanner
+	scanner := bufio.NewScanner(file)
+
+	// For line in file, add it to the allowed_tokens slice
+	for scanner.Scan() {
+		allowed_tokens = append(allowed_tokens, scanner.Text())
+	}
+
+	//
+	for _, line := range allowed_tokens {
+		if auth_token == line {
+			slack_post(json_data.Action, json_data.LogMessage)
+		} else {
+			LOG_FILE := "/path/to/invalid.log"
+			f, err := os.OpenFile(LOG_FILE, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0640)
+			if err != nil {
+				error_func(err)
+			}
+			_, err = f.WriteString("Invalid Auth Token: " + auth_token)
+			if err != nil {
+				error_func(err)
+			}
+		}
+	}
+
+}
+	
+func slack_post(action, LogMessage string) {
 	// define Slack API
 	slack_api := slack.New("Bot token")
 	CHANNEL_ID := "#sftp-alerts"
-
+	// Define these outside of if blocks
 	var slack_attachment_color string
-
 	var slack_title string
 
-	// If the value of Auth field in JSON is in the allowed_tokens map
-	if allowed_tokens[json_data.Auth] {
-
-		// Debugging: Just pring the LogMessage
-		//fmt.Println(json_data.LogMessage)
-		
-		// Is this a success or error?
-		if json_data.Action == "Success" {
-			slack_attachment_color = "good"
-			slack_title = "SFTP Success"
-			fmt.Println("good")
-		} else {
-			slack_attachment_color = "danger"
-			slack_title = "SFTP Error"
-			fmt.Println("danger")
-		}
-
-		slack_long_message := json_data.LogMessage
-		slack_attachment := slack.Attachment{
-			Color: slack_attachment_color,
-			Text: slack_long_message,
-				Fields: []slack.AttachmentField{
-					slack.AttachmentField{
-						Title: slack_title,
-					},
-				},
-		}
-		fmt.Println(CHANNEL_ID)
-		channelID, timestamp, err := slack_api.PostMessage(CHANNEL_ID, slack.MsgOptionAttachments(slack_attachment))
-		if err != nil {
-			LOG_FILE := "/path/to/log.error"
-			slack_fail_prefix := "Message failed to send to channel "
-
-			f, err := os.OpenFile(LOG_FILE, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0640)
-			error_func(err)
-
-			_, err = f.WriteString(slack_fail_prefix + channelID + " " + timestamp)
-			error_func(err)
-
-		} else{
-			LOG_FILE := "/path/to/log.log"
-			slack_success_prefix := "Message successfully sent to channel: "
-
-			f, err := os.OpenFile(LOG_FILE, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0640)
-			error_func(err)
-
-			defer f.Close()
-
-			_, err = f.WriteString(slack_success_prefix + channelID + timestamp)
-			error_func(err)
-		}
-
+	if action == "Success" {
+		slack_attachment_color = "good"
+		slack_title = "SFTP Success"
 	} else {
-		fmt.Println("Invalid Auth Token")
+		slack_attachment_color = "danger"
+		slack_title = "SFTP Error"
 	}
 
+	slack_long_message := LogMessage
+	slack_attachment := slack.Attachment{
+		Color: slack_attachment_color,
+		Text: slack_long_message,
+			Fields: []slack.AttachmentField{
+				slack.AttachmentField{
+					Title: slack_title,
+				},
+			},
+	}
+	channelID, timestamp, err := slack_api.PostMessage(CHANNEL_ID, slack.MsgOptionAttachments(slack_attachment))
+	if err != nil {
+		LOG_FILE := "/path/to/log.error"
+		slack_fail_prefix := "Message failed to send to channel "
 
-	
+		f, err := os.OpenFile(LOG_FILE, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0640)
+		error_func(err)
+
+		_, err = f.WriteString(slack_fail_prefix + channelID + " " + timestamp)
+		error_func(err)
+
+	} else {
+		LOG_FILE := "/path/to/log.log"
+		slack_success_prefix := "Message successfully sent to channel: "
+
+		f, err := os.OpenFile(LOG_FILE, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0640)
+		error_func(err)
+
+		defer f.Close()
+
+		_, err = f.WriteString(slack_success_prefix + channelID + timestamp)
+		error_func(err)
+	} 
+
+
 }
 
 func main() {
